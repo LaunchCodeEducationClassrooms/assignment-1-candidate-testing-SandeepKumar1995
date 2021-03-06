8ae11260623fd12daaf22c4fac7304636e5cab34
SandeepKumar1995 <54086388+SandeepKumar1995@users.noreply.github.com>
2021-03-06 16:01:25 -0500

    Update candidate-testing.js
    Removed code that was not necessary

--- candidate-testing.js
+++ candidate-testing.js
@@ -1,52 +1,11 @@
 const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 // TODO 1.1a: Define candidateName //
-console.log("Welcome to part 1: Minimum Viable Quiz");
-console.log(" ");
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 var question;
 var correctAnswer;
 var candidateAnswer;
-var questions;
-var correctAnswers;
-var candidateAnswers; 
 var candidateName =" ";
- //function askForName() {
-  // TODO 1.1b: Ask for candidate's name //
-  function askForName() {
-// TODO 1.1b: Ask for candidate's name //
-var candidateName = input.question("Please enter your Name ", "candidateName");
-console.log(" ");
-console.log("Welcome to the test ", candidateName);
-}
- askForName();
-//function askQuestion() {
-  // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-  console.log(" ");
-var candidateAnswer = (input.question("Who was the first American woman in space? ", "candidateAnswer")).toLowerCase();
-  correctAnswer = candidateAnswer;
-while(true)
-{
-  if (correctAnswer ==="sally ride")
-  {
-    console.log(" ");
-  console.log("Congratulations your answer " + candidateAnswer + " is correct");
-  break;
-  }
-  else
-  {
-    correctAnswer = input.question("Please check your answer and attempt again ", "correctAnswer");
-  }
-}
-console.log(" ");
-console.log("End of Part one");
-console.log(" ");
-console.log(" ");
-//}
-console.log("Welcome to part 2 and 3: Multiple Questions");
-console.log(" ");
-
-// TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 var questions = [[ 'Who was the first American woman in space?'], 
 ['True or false: 5000 meters = 5 kilometers.'], 
 ["Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?"],
@@ -56,29 +15,36 @@ var correctAnswers = [['sally ride'],['true'],['trajectory'], ['40'], ['3']];
 var candidateAnswers = [];
 var gradeQuiz = 0;
 
+// TODO 1.1b: Ask for candidate's name //
+function askForName() {
+  var candidateName = input.question("Please enter your Name ", "candidateName");
+  console.log(" ");
+  console.log("Welcome to the test ", candidateName);
+}
+//function askQuestion() {
+// TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
+console.log("Welcome to part 2 and 3: Multiple Questions");
+console.log(" ");
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
   var i=0;
-    while (i < questions.length){ 
-   
-      candidateAnswers.push(input.question(i+1 + ")  " + questions[i]));
-      if((correctAnswers[i]) == (candidateAnswers[i]).toLowerCase()){
-        gradeQuiz+=1;
-      }
-      console.log("Candidate Answer  " + candidateAnswers[i])
-      console.log("Correct Answer " + correctAnswers[i])
-      console.log(" ");
-         i++; 
-
-}
+  while (i < questions.length){ 
+    candidateAnswers.push(input.question(i+1 + ")  " + questions[i]));
+    if((correctAnswers[i]) == (candidateAnswers[i]).toLowerCase()){
+      gradeQuiz+=1;
+    }
+    console.log("Candidate Answer  " + candidateAnswers[i])
+    console.log("Correct Answer " + correctAnswers[i])
+    console.log(" ");
+    i++; 
+  }
 }
 
 function runProgram() {
   // TODO 1.1c: Ask for candidate's name //
   askForName();
   console.log(" ");
-  
   askQuestion();
   //gradeQuiz(this.candidateAnswers);
 if (gradeQuiz >= 2){
